dc8e68f2b9f557fa63b02739f8f60b417217cde9
Mads Lundsby Bisgaard <mads@malbi.dk>
2023-06-21 17:54:14 +0200

    Your-combos-&-abilities-section (#6)
    * Done
    
    Missing video for now
    
    * Add videos showcasing the different abilites
    
    * Update script.js

---
+++ features/combo/script.js
@@ -0,0 +1,32 @@
+const buttons = document.querySelectorAll('.combo-button');
+const videoPlayer = document.getElementById('combo-video');
+let isVideoPlaying = false;
+let isDelayActive = false;
+
+buttons.forEach((button) => {
+  button.addEventListener('mouseover', () => {
+    if (isDelayActive) return;
+
+    const videoUrl = button.dataset.videoUrl;
+
+    if (videoPlayer.src !== videoUrl) {
+      videoPlayer.src = videoUrl;
+      videoPlayer.load();
+    }
+
+    videoPlayer.play();
+    isVideoPlaying = true;
+    videoPlayer.parentNode.classList.add('show-video');
+  });
+
+  button.addEventListener('mouseout', () => {
+    isVideoPlaying = false;
+    videoPlayer.parentNode.classList.remove('show-video');
+
+    // Delay to prevent flickering
+    isDelayActive = true;
+    setTimeout(() => {
+      isDelayActive = false;
+    }, 300);
+  });
+});
